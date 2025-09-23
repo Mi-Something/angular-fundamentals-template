ed2f83280a41918b89de1e22d7d8c10a8182502c
Misha Ripa <ripami25@gmail.com>
2025-09-23 17:21:42 +0300

    Fixes after task 3 autocode attempts

--- src/app/shared/components/login-form/login-form.component.ts
+++ src/app/shared/components/login-form/login-form.component.ts
@@ -1,19 +1,20 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss'],
+  selector: "app-login-form",
+  templateUrl: "./login-form.component.html",
+  styleUrls: ["./login-form.component.scss"],
 })
 export class LoginFormComponent {
   @ViewChild("loginForm") public loginForm!: NgForm;
-
+  submitted = false;
   onSubmit(form: NgForm) {
+    this.submitted = true;
     if (form.valid) {
-      console.log('Form values:', form.value);
+      console.log("Form values:", form.value);
     } else {
-      console.log('Form is invalid');
+      console.log("Form is invalid");
     }
   }
 
