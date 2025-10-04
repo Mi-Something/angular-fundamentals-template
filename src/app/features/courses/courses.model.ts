c1f51d7b3c2ba0a068b0ce721c1154ac1b0b6616
Misha Ripa <ripami25@gmail.com>
2025-10-04 16:07:13 +0300

    Task 5. Adding disappeared effects

--- src/app/features/courses/courses.model.ts
+++ src/app/features/courses/courses.model.ts
@@ -1,15 +1,12 @@
 export interface Course {
   id: string;
   title: string;
-  description: string;
-  creationDate: string;
-  duration: number;
-  authors: string[];
+  description?: string;
+  creationDate?: string;
+  duration?: number;
+  authors?: string[];
 }
-export interface CoursesError {
-  message: string;
-  code?: number;
-}
+export type CoursesError = string;
 
 export interface CoursesResponse {
   successful: boolean;
